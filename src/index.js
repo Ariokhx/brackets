1d6e95958af8ad077e1238e2945fd818b78ec4fc
Ariokhx <Vladdrob@gmail.com>
2021-03-16 02:01:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  while (true) {
+    let strL = str.length;
+    bracketsConfig.forEach(Pair => {
+      str = str.replace(Pair[0] + Pair[1], '')
+    });
+    if (strL == str.length) {
+        break;
+    }
+  }
+    return str.length == 0;
 }
